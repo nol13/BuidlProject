70cd8615a1339d92eb2bb0fcdacf03470d828135
Nolan Kaplan <nolan.kaplan@constructconnect.com>
2023-02-28 10:13:46 -0500

    add arbitrum

--- src/pages/posts/[uid].tsx
+++ src/pages/posts/[uid].tsx
@@ -3,8 +3,9 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 type Posts = {
-    uid: number
+    uid: string
 }
+
 const UserPosts: React.FC<Posts> = () => {
     const router = useRouter();
     const { uid } = router.query;
