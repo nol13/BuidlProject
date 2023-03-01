b12bc00aaccc8fb8ea8cd09b32af57850db6519c
Nolan Kaplan <nolan.kaplan@constructconnect.com>
2023-03-01 17:25:30 -0500

    emit events, additional functions

--- truffle/test/simplestorage.js
+++ truffle/test/simplestorage.js
@@ -1,6 +1,6 @@
 let simpleStorageInstance;
 
-const SimpleStorage = artifacts.require("SimpleStorage");
+const SimpleStorage = artifacts.require("Simple");
 
 contract("SimpleStorage", accounts => {
 
@@ -10,10 +10,10 @@ contract("SimpleStorage", accounts => {
 
   it("...should store the value 89.", async () => {
     // Set value of 89
-    const tx = await simpleStorageInstance.set(89, { from: accounts[0]});
+    const tx = await simpleStorageInstance.createPost('wf', 'wrgwe', 45645, 0x6f3c67DA2827aaE123d1379939E2E7294d62C06B, { from: accounts[0]});
 
     // Get stored value
-    const storedData = await simpleStorageInstance.get.call();
+    const storedData = await simpleStorageInstance.posts.call(0);
 
     assert.equal(storedData, 89, "The value 89 was not stored.");
   });
