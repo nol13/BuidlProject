c90b72b8be64e013e410b55a74457d601eb38845
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-03-01 12:54:53 -0700

    intial form created

--- src/pages/createpost.tsx
+++ src/pages/createpost.tsx
@@ -1,5 +1,19 @@
 const CreatePost = () => {
-  return <div>HI</div>;
+  return (
+    <div className="flex flex-col items-center space-y-6">
+      <p className="text-4xl">Create Your Article Here</p>
+      <input
+        type="text"
+        placeholder="Title"
+        className="input input-primary w-full max-w-xs input-lg"
+      />
+      <textarea
+        className="textarea w-2/4 textarea-primary textarea-lg h-[400px]"
+        placeholder="Main Text"
+      ></textarea>
+      <button className="btn btn-primary">Post</button>
+    </div>
+  );
 };
 
 export default CreatePost;
