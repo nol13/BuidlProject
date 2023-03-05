59c9b8f39878b0afa764a0a76383d1f7ff4734f3
Nolan Kaplan <nolan.kaplan@constructconnect.com>
2023-03-05 07:01:01 -0500

    npm for build command

--- src/components/postCollection.tsx
+++ src/components/postCollection.tsx
@@ -16,7 +16,6 @@ type PostProps = {
 
 export default function PostCollection({ posts }: PostProps) {
 
-
   return (
     <div className="container flex flex-col m-auto gap-4 max-w-prose">
       {posts.map((post) => {
